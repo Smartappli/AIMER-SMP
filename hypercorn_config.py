ab3f873c586367c8161aef733b6a382070959e66
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-23 19:30:37 +0200

    Create hypercorn_config.py

---
+++ hypercorn_config.py
@@ -0,0 +1,27 @@
+import anyio
+import os
+from dotenv import load_dotenv
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+from main import (
+    app,
+)
+
+load_dotenv()
+
+
+def load_config() -> Config:
+    bind_addresses = os.getenv("BIND", "0.0.0.0:8000").split(",")
+    config = Config()
+    config.bind = bind_addresses
+    return config
+
+
+async def main():
+    config = load_config()
+    async with anyio.create_task_group() as task_group:
+        await task_group.spawn(serve, app, config)
+
+
+if __name__ == "__main__":
+    anyio.run(main)
