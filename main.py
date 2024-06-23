5898a85bc92530b4145ce196dcb4a2747a38b7e6
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-23 15:14:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,15 @@ class FpnParams(BaseModel):
     encoder_name: str = "resnet34"
     encoder_depth: int = 5
     encoder_weights: str | None = "imagenet"
-    decoder_pyramid_channels=256, decoder_segmentation_channels=128, decoder_merge_policy='add', decoder_dropout=0.2, in_channels=3, classes=1, activation=None, upsampling=4, aux_params=None
+    decoder_pyramid_channels: int = 256
+    decoder_segmentation_channels: int = 128
+    decoder_merge_policy: str = "add"
+    decoder_dropout: float = 0.2
+    in_channels: int = 3, 
+    classes: int = 1
+    activation: str | None = None
+    upsampling: int = 4
+    aux_params: dict | None = None
 
 
 class MAnetParams(BaseModel):
