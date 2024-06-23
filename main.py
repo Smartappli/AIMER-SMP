f2da35c43deaa22d35a96331f30887f8e4d073ef
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-23 20:13:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ class FpnParams(BaseModel):
     decoder_segmentation_channels: int = 128
     decoder_merge_policy: str = "add"
     decoder_dropout: float = 0.2
-    in_channels: int = 3, 
+    in_channels: int = 3
     classes: int = 1
     activation: str | None = None
     upsampling: int = 4
